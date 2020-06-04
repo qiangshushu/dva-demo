a146836c840dabdc0001f60f035e01fd0322c485
qiangshushu <Nova001587@novastar.com>
2020-06-04 10:53:54 +0800

    init

---
+++ src/routes/Products.js
@@ -0,0 +1,20 @@
+import React from "react";
+import { connect } from "dva";
+import ProductList from "../components/ProductList";
+
+const Products = ({ dispatch, products }) => {
+  function handleDelete(id) {
+    dispatch({
+      type: "products/delete",
+      payload: id,
+    });
+  }
+  return (
+    <div>
+      <h2>List of Products</h2>
+      <ProductList onDelete={handleDelete} products={products}></ProductList>
+    </div>
+  );
+};
+
+export default connect(({ products }) => ({ products }))(Products);
